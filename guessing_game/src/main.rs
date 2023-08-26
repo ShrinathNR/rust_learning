e2b26b1fc73d93065df8dcc79b455c4e231d568f
ShrinathNR <nr.shrinath@gmail.com>
2023-08-26 11:59:05 +0530

    add guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,20 +5,30 @@ fn main() {
     println!("guess a number: ");
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("secret number {secret_number}");
-    println!("enter your guess number from 1 to 100");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-    let guess : u32 = guess.trim().parse().expect("type a number");
 
-    println!("you guessed: {guess}");
+    loop{
+        println!("enter your guess number from 1 to 100");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess : u32 =match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed: {guess}");
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("too small"),
-        Ordering::Greater => println!("too big"),
-        Ordering::Equal => println!("you win"),
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
     }
+
+    
 
 
 }
