f68157a5cd3e57b3b72cf6998e7f2105833f34e9
ShrinathNR <nr.shrinath@gmail.com>
2023-12-20 18:30:18 +0530

    add macro vec

--- adv_concepts/src/macros.rs
+++ adv_concepts/src/macros.rs
@@ -5,7 +5,19 @@ pub fn macros () {
 // declarative macros are similar to match
 // expcept we match for rust code and return a rust code
 
-macro_rules! four {
-    () => { 3+1 };
+// macro_rules! four {
+//     () => { 3+1 };
+// }
+
+//simple vec macros
+#[macro_export]
+macro_rules! vec {
+    ($($x:expr),*) => {
+        let temp_vec = Vec::new();
+        $(
+            temp_vec.push($x);
+        )*
+        temp_vec
+    };
 }
 
